c2a2dbb4ca7b638236a2f5567e122ceba1320191
Shreyans207 <75474973+Shreyans207@users.noreply.github.com>
2021-09-08 22:01:16 +0530

    Update atm.py

--- atm.py
+++ atm.py
@@ -1,29 +1,49 @@
 import os
-import  shutil
+import shutil
 import random
 
-disclaimer = print('Disclaimer :- This is an hypothetical game, no fraud has been made in someones account, everything is fake, it is for learning new concepts of functions in python by white hat jr.')
-question = bool(input('Do you want to continue?? : '))
+class atm :
+   def __init__(self,card_number,pin_number,transaction,accountBalance) : 
+      self.card_number = card_number
+      self.pin_number = pin_number
+      self.transaction = transaction
+      self.accountBalance = accountBalance
+      
+def main() :
+   disclaimer = print('Disclaimer :- This is an hypothetical game, no fraud has been made in someones account, everything is fake, it is for learning new concepts of functions in python')
+   card_number =  int(input("Enter your bank card no... : "))
+   pin_number =  int(input("Enter your bank pin no...  : "))
+   accountBalance = 0
+   print(f'Current Account Balance : {accountBalance} Rs.')
+   loop = True
 
-if (question == true) : 
-    card_number =  int(input("Enter your bank card no... (account balance depends on it ;) : "))
-    pin_number =  int(input("Enter your bank pin no... (account balance depends on it ;) : "))
-    accountBalance = card_number/pin_number + random.randint(1000,10000)
-    balance_currency = print(f'Account Balance = {acountBalance} Rs.')
-    transaction = int(input("Donate Money (in rs.) : "))
+   accountBalance = int(input('Make deposit in your account ? : '))
+   change = print('Activity Successfull')
+   balance_currency = print(f'Account Balance = {accountBalance } Rs.')
 
-    class atm : 
-       def __init__(self,card_number,pin_number,transaction,accountBalance) : 
-          self.card_number = card_number
-          self.pin_number = pin_number
-          self.transaction = transaction
-          self.accountBalance = accountBalance
+   while(loop) : 
+       
+      transaction = int(input("Donate Money : "))
 
-       def transaction(self):
-          if (self.transaction > transaction) : 
-           print('YAY! Transaction completed')
-           accountBalance = accountBalance - transaction
-          else : 
-           print(f'Sorry, Not enough money in the account.. you have only {balance_currency} in your account')
-else : 
-    print('Well.... Thank you :)')
+      if (accountBalance > transaction) : 
+         print('YAY! Transaction completed')
+         accountBalance = accountBalance - transaction
+         print(f'Account Balance : {accountBalance}')
+
+         question2 = bool(input('Wanna play more ? : '))
+
+         if (question2 == False) : 
+            loop = False
+            print('Thanks for playing')
+            rate = input('Rate Us : ')
+         else : 
+            loop = True
+      else : 
+         print(f'Sorry, Not enough money in the account.. you have only {accountBalance} in your account')
+         accountBalance2 = accountBalance + int(input('Make deposit in your account ? : '))
+         accountBalance = accountBalance2
+         change2 = print('Activity Successfully')
+         balance_currency = print(f'Account Balance = {accountBalance} Rs.')
+
+if __name__ == '__main__' : 
+    main()
